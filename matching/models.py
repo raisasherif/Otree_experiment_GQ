b834c4fa475ce327c9bc42eb54589725a73d58d8
michael-pont <35072335+michael-pont@users.noreply.github.com>
2018-07-24 13:10:25 +0200

    table implemented, total deviation calculated

--- matching/models.py
+++ matching/models.py
@@ -22,16 +22,14 @@ class Constants(BaseConstants):
 class Subsession(BaseSubsession):
     def creating_session(self):
         for p in self.get_players():
-            print('new player')
             randomized_students = random.sample(Constants.students, len(Constants.students))
             p.participant.vars['all_students'] = randomized_students
             p.participant.vars['selected_students'] = p.select_students()
-            print('sdsdsdsdsds')
-            print(p.participant.vars)
-            # p.age = int(student_data['id'])
-            # print(p.age)
-
-
+            p.actual_1 = p.participant.vars['selected_students'][0]['score']
+            p.actual_2 = p.participant.vars['selected_students'][1]['score']
+            p.actual_3 = p.participant.vars['selected_students'][2]['score']
+            p.actual_4 = p.participant.vars['selected_students'][3]['score']
+            p.actual_5 = p.participant.vars['selected_students'][4]['score']
 
 class Group(BaseGroup):
     pass
@@ -43,9 +41,32 @@ class Player(BasePlayer):
     institute = models.StringField()
     state = models.StringField()
     language = models.StringField()
+    prediction_1 = models.IntegerField(min=0, max=20, label="")
+    prediction_2 = models.IntegerField(min=0, max=20, label="")
+    prediction_3 = models.IntegerField(min=0, max=20, label="")
+    prediction_4 = models.IntegerField(min=0, max=20, label="")
+    prediction_5 = models.IntegerField(min=0, max=20, label="")
+    actual_1 = models.IntegerField(min=0, max=20)
+    actual_2 = models.IntegerField(min=0, max=20)
+    actual_3 = models.IntegerField(min=0, max=20)
+    actual_4 = models.IntegerField(min=0, max=20)
+    actual_5 = models.IntegerField(min=0, max=20)
+    total_deviation = models.IntegerField(initial=0, min=0)
+    
     #submitted_answer = models.StringField(widget=widgets.RadioSelect)
     #is_correct = models.BooleanField()
 
     def select_students(self):
-        print( [self.participant.vars['all_students'][i] for i in range(0,5)])
         return [self.participant.vars['all_students'][i] for i in range(0,5)]
+
+    def calculate_deviation(self):
+        comparison_list = [
+            (self.prediction_1, self.actual_1),
+            (self.prediction_2, self.actual_2),
+            (self.prediction_3, self.actual_3),
+            (self.prediction_4, self.actual_4),
+            (self.prediction_5, self.actual_5)
+        ]
+
+        for pair in comparison_list:
+            self.total_deviation += abs(pair[0] - pair[1])
--- matching/pages.py
+++ matching/pages.py
@@ -4,12 +4,16 @@ from .models import Constants
 
 
 class my_match(Page):
-    # form_model = 'player'
-    # form_fields = ['guess_1','guess_2','guess_3','guess_4','guess_5']
+    form_model = 'player'
+    form_fields = ['prediction_1','prediction_2','prediction_3','prediction_4','prediction_5']
+
     def vars_for_template(self):
         return {
             'students': self.participant.vars['selected_students'],
         }
+
+    def before_next_page(self):
+        self.player.calculate_deviation()
 
 class Results(Page):
     pass
